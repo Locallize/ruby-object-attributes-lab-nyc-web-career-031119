ccb3fc90dc73734f94ae0c63d9bc0a5209ea9dfc
Fillan Lee <fillanlee@aol.com>
2019-03-13 02:38:45 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,11 +1,4 @@
-
-# class Person
-#     def name=(name)
-#         @this_dogs_name = dogs_name
-#     end
-    
-#     def name
-#         @this_dogs_name
-#     end
-# end
-
+class Person
+  
+  def $FILENAME
+end
