2818fd18e6c52fb467bf2e1cd4d42b73494ccbf4
Fillan Lee <fillanlee@aol.com>
2019-03-13 02:30:46 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+    
+      # def initialize name
+ 
+        def name=(dogs_name)
+          @this_dogs_name = dogs_name
+        end
+       
+        def name
+          @this_dogs_name
+        end
+        
+        def breeds
+          
+        end
+      
+end
+
+Fido = Dog.new
+Fido.name = "Fido"
